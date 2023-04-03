d0f3c4ffe136da5e73692bae43de1fc5e38c5987
Sadia Gray <sadiagray17@gmail.com>
2023-04-02 23:39:59 -0500

    completed mvp

--- src/App.js
+++ src/App.js
@@ -5,15 +5,24 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React , { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import Posts from './components/Posts/Posts';
+import Post from './components/Posts/Post';
+import PostHeader from './components/Posts/PostHeader';
+import SearchBar from './components/SearchBar/SearchBar';
+import LikeSection from './components/Posts/LikeSection';
 // Import the dummyData
+import dummyData from './dummy-data';
 import './App.css';
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  const [posts, setPosts] = useState(dummyData);
+  const [searchTerm, setSearchTerm] = useState("");
+  console.log(posts);
 
   const likePost = postId => {
     /*
@@ -27,12 +36,31 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    const likes = posts.map(e => {
+      if (e.id === postId) {
+        return { ...e, likes: e.likes + 1 };
+      } else {
+        return e;
+      }
+    });
+    setPosts(likes);
+  };
+  const searchUser = () => {
+    const termNormalizer = searchTerm.trim().toLowerCase();
+    if (!termNormalizer) return posts;
+    return posts.filter((user) => {
+      return user.username.toLowerCase().includes (termNormalizer);
+    });
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       {/* Check the implementation of each component, to see what props they require, if any! */}
+      <SearchBar />
+      <Posts posts={posts} likePost={likePost} />
+      <SearchBar setSearchTerm = {setSearchTerm} />
+      <Posts posts = {searchUser()} likePost = {likePost} />
     </div>
   );
 };
